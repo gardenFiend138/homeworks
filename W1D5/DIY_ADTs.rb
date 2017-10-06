e2eefa5e88fa3befb36d75c96309517d2050cf5c
Guy Wassather <ggwass@gmail.com>
2017-10-05 22:43:04 -0700

    Create custom ADT classes

---
+++ W1D5/DIY_ADTs.rb
@@ -0,0 +1,76 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack.push(el)
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.push(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+
+    @map.each do |pair|
+      if pair[0] == key
+        pair[1] = value
+        return
+      end
+    end
+    
+    @map << [key, value]
+
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      return pair.last if pair.first == key
+    end
+    nil
+  end
+
+  def remove(key)
+    @map.each_with_index do |pair, idx|
+      if pair.first == key
+        @map.delete_at(idx)
+        return
+      end
+    end
+  end
+
+  def show
+    @map
+  end
+
+end
