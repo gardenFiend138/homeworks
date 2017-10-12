db621b343f831ce7997298f925f8690883a77cd8
Guy Wassather <ggwass@gmail.com>
2017-10-11 22:09:18 -0700

    Create search and sort algorithms with given worst-case time complexity

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -11,9 +11,15 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 # back to check the solutions to get them all to pass
 
 describe Dessert do
+  # Here we set up our dummy object (mock) as an instance of the dessert class
+  # above it, we also et up a dummy chef class so that we ca interact with
+  # the chef class when calling interdependent methods
   let(:chef) { double("chef", name: "Guy") }
   subject(:dessert) {Dessert.new("cake", 138, chef) }
 
+  # Here we are testing the initialize function; notice this syntax:
+  # describe "string that describes action" do
+  # expect(call method on mock instanct).to eq(result, matching data type)
   describe "#initialize" do
     it "sets a type" do
       expect(dessert.type).to eq("cake")
@@ -23,9 +29,16 @@ describe Dessert do
       expect(dessert.quantity).to eq(138)
     end
 
+    # A better way to do this would be to see if the ingredients is_a?(Array)
+    # and then checking to see if it is also empty?
+
     it "starts ingredients as an empty array" do
       expect(dessert.ingredients).to eq([])
     end
+
+    # Notice the block style {} for raising errors; also, have to actually
+    # call the initialize method with the required arguments for this type
+    # of test
 
     it "raises an argument error when given a non-integer quantity" do
       expect { Dessert.new("cake", "one", "Guy") }.to raise_error(ArgumentError)
@@ -34,10 +47,14 @@ describe Dessert do
 
   describe "#add_ingredient" do
     it "adds an ingredient to the ingredients array" do
-      dessert.add_ingredient("icing")
-      expect(dessert.ingredients).to include("icing")
+      dessert.add_ingredient("icing") # here we call the method with the argument
+      expect(dessert.ingredients).to include("icing") # and we check
     end
   end
+
+  # The first comment within this test was my first attempt; we can think
+  # of writing tests like writing methods--what we do within the test is
+  # limited in scope to the  test in which it was defined
 
   describe "#mix!" do
     it "shuffles the ingredient array" do
@@ -49,7 +66,7 @@ describe Dessert do
       end
 
       expect(dessert.ingredients).to eq(ingredients)
-      dessert.mix!
+      dessert.mix! # since the method calls mix, we have to call it too;
       expect(dessert.ingredients).not_to eq(ingredients)
       expect(dessert.ingredients.sort).to eq(ingredients.sort)
     end
@@ -57,15 +74,19 @@ describe Dessert do
 
   describe "#eat" do
     it "subtracts an amount from the quantity" do
-      dessert.eat(13)
+      dessert.eat(13) # calling method on mock object with argument
       expect(dessert.quantity).to eq(125)
     end
+
+    # again, notice the block style for raising errors
 
     it "raises an error if the amount is greater than the quantity" do
       expect { dessert.eat(139) }.to raise_error("not enough left!")
     end
   end
 
+  # here is where we're testing the methods that are dependent on the
+  # chef class; notice the allow(class instance).to receive(:method).and_return("result")
   describe "#serve" do
     it "contains the titleized version of the chef's name" do
       allow(chef).to receive(:titleize).and_return("Guy the Great Baker")
@@ -73,6 +94,10 @@ describe Dessert do
     end
   end
 
+  # Notice how we do something similar here, but then after the interdependent
+  # method call, we call the method #make_more, which calls chef to do some
+  # baking;
+   
   describe "#make_more" do
     it "calls bake on the dessert's chef with the dessert passed in" do
       expect(chef).to receive(:bake).with(dessert)
---
+++ W2D4/big_o.rb
@@ -0,0 +1,109 @@
+# Find longest string in array in O(n^2); nested loops; bubble sort
+
+def sluggish_octopus(arr)
+
+  sorted = false
+  until sorted
+    sorted = true
+    arr.each_index do |idx|
+      next if idx + 1 > arr.length - 1
+      if arr[idx].length > arr[idx + 1].length
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+        sorted = false
+      end
+    end
+  end
+
+  arr.last
+end
+
+p sluggish_octopus(["a", "abcd", "abcdef", "ab", "abc", "abcde"])
+
+# Find longest string in array in O(n log n)); merge sort; quick sort
+# How to return the last element from this recursive sort?
+# def dominant_octopus(arr)
+#   return arr if arr.length < 2
+#
+#   prc = Proc.new { |x, y| x <=> y }
+#   pivot = arr.first
+#
+#   left = arr[1..-1].select { |el| prc.call(el, pivot) == -1 }
+#   right = arr[1..-1].reject { |el| prc.call(el, pivot) == -1 }
+#   dominant_octopus(left) + [pivot] + dominant_octopus(right)
+#
+# end
+
+def dominant_octopus(arr)
+  return arr if arr.length < 2
+  mid = arr.length / 2
+
+  sorted_left = dominant_octopus(arr.take(mid))
+  sorted_right = dominant_octopus(arr.drop(mid))
+
+  merge(sorted_left, sorted_right)
+end
+
+def merge(arr1, arr2)
+  merged = []
+  until arr1.empty? || arr2.empty?
+    case arr1.first <=> arr2.first
+    when -1
+      merged << arr1.shift
+    when 0
+      merged << arr1.shift
+    when 1
+      merged << arr2.shift
+    end
+  end
+  merged.concat(arr1)
+  merged.concat(arr2)
+  merged
+end
+
+
+p dominant_octopus(["a", "abcd", "abcdef", "ab", "abc", "abcde"])
+
+# Find longest string in array in O(n)
+
+def clever_octopus(arr)
+  longest = arr.first
+
+  arr.each do |el|
+    longest = el if el.length > longest.length
+  end
+  longest
+end
+
+p clever_octopus(["a", "abcd", "abcdef", "ab", "abc", "abcde"])
+
+# Find longest string in array in O(n)
+
+def slow_dance(direction, tiles_array)
+
+  tiles_array.each_with_index do |dir, idx|
+    return idx if direction == dir
+  end
+  nil
+end
+
+p slow_dance("up", ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ])
+
+# Find longest string in array in O(1); binary search
+
+def constant_dance(direction, hash)
+  hash.each do |key, value|
+    return key if value == direction
+  end
+  nil
+end
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+dir_hash = {}
+i = 0
+tiles_array.each do |dir|
+  dir_hash[i] = dir
+  i += 1
+end
+
+p constant_dance("right", dir_hash)
