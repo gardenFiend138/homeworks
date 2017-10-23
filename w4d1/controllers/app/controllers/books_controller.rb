a085937d2fd7a0dde86720418d8824ff6fa0da31
Guy Wassather <ggwass@gmail.com>
2017-10-22 17:07:26 -0700

    Build BooksController for library

--- w4d1/controllers/app/controllers/books_controller.rb
+++ w4d1/controllers/app/controllers/books_controller.rb
@@ -1,18 +1,35 @@
+=begin
+I spent a while trying to tinker with my code to even get the index to work;
+Things are starting to come together though: I'm seeing how classes are
+interacting with one another in practicel applications, such as with routers,
+controllers, and methods. The syntax looked familiar from the videos, but
+I wasn't able ot get it on my own.
+=end
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
+    render :index
   end
 
   def new
-    # your code here
+    Book.new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params)
+
+    if book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = books.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
+    book = Book.find_by(params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
