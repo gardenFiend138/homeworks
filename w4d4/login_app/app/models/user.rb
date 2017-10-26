1e0fadbe649e3bdaee228917c7ada5642157c265
Guy Wassather <ggwass@gmail.com>
2017-10-26 08:38:56 -0700

    Fix user model

--- w4d4/login_app/app/models/user.rb
+++ w4d4/login_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, prsesence: { message: "Password can't be blank" }
+  validates :password_digest, presence: { message: "Password can't be blank" }
   validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true}
   before_validation :ensure_session_token
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   #   pass.length { min: 6, allow_nil: true }
   # end
 
-  def find_by_credentials(user_name, password)
+  def find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user &&
     BCrypt::Password.new(user.password_digest).is_password?(password)
@@ -24,7 +24,7 @@ class User < ApplicationRecord
 
 # forgot the self here...
   def self.generate_session_token
-    SecureRandom::urlsafe_base_64(16) # => 16 is the default if n isn't specified
+    SecureRandom::urlsafe_base64(16) # => 16 is the default if n isn't specified
   end
 
   def reset_session_token
@@ -40,7 +40,7 @@ class User < ApplicationRecord
     self.session_token ||= User.generate_session_token
   end
 
-  def password=(pass)
+  def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
     # @password = BCrypt::Password.create(pass)
--- w4d4/login_app/db/migrate/20171026135235_create_users.rb
+++ w4d4/login_app/db/migrate/20171026135235_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username, presence: true, uniqueness: true
-      t.string :password_digest, presence: true
-      t.string :session_token
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
 
       t.timestamps
     end
-    add_index(:users, [:username, :session_token], unique: true)
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+    # add_index(:users, [:username, :session_token], unique: true)
   end
 end
