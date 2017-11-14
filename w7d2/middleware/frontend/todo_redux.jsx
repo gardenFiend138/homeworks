c804e3378dd75b0ce9b76a1048c4dec26903e544
Guy Wassather <ggwass@gmail.com>
2017-11-13 22:25:54 -0800

    Refactor store logging using middleware

--- w7d2/middleware/frontend/todo_redux.jsx
+++ w7d2/middleware/frontend/todo_redux.jsx
@@ -8,17 +8,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  // store.dispatch = addLoggingToDispatch(store); // Step 1--Logging store
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
-const addLoggingToDispatch = (store) => {
-  const storeDispatch = store.dispatch;
-  return (action) => {
-    console.log(store.getState());
-    console.log(action);
-    storeDispatch(action);
-    console.log(store.getState());
-  };
-};
+// Step 1--Logging our store
+// const addLoggingToDispatch = (store) => {
+//   const storeDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     storeDispatch(action);
+//     console.log(store.getState());
+//   };
+// };
+
+// Step 2-- Creating middleware (ES6 notation) (curried function)
+const addLoggingToDispatch = store => next => action {
+
+}
+
+// Step 2--Creating middleware; below is ES5 notation; we will be using
+// ES6 arrow notation (see above).
+
+// function addLoggingToDispatch(store) {
+//   return function(next) {
+//     return function(action) {
+//
+//     };
+//   };
+// };
