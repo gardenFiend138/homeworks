2da52931e62854e84f0167c372df723b2d6ee662
Guy Wassather <ggwass@gmail.com>
2017-11-13 22:43:10 -0800

    Apply middleware to store

--- w7d2/middleware/frontend/todo_redux.jsx
+++ w7d2/middleware/frontend/todo_redux.jsx
@@ -8,7 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+
   // store.dispatch = addLoggingToDispatch(store); // Step 1--Logging store
+  // store = applyMiddleWares(store, addLoggingToDispatch); // refactor step 1
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
@@ -26,9 +28,22 @@ document.addEventListener('DOMContentLoaded', () => {
 // };
 
 // Step 2-- Creating middleware (ES6 notation) (curried function)
-const addLoggingToDispatch = store => next => action {
+// moved to './store/store.js'
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// };
 
-}
+const applyMiddleWares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+
+  return Object.assign({}, store, { dispatch });
+};
 
 // Step 2--Creating middleware; below is ES5 notation; we will be using
 // ES6 arrow notation (see above).
