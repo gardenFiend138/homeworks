750715efec8ad3c5b3b0058863f0ef6f7ba8dc8f
Guy Wassather <ggwass@gmail.com>
2017-11-15 20:48:50 -0800

    Add links and routes to Rainbow class

--- w7d4/rainbowRoutes/components/rainbow.jsx
+++ w7d4/rainbowRoutes/components/rainbow.jsx
@@ -15,10 +15,17 @@ class Rainbow extends React.Component {
     return (
       <div>
         <h1>Rainbow Router!</h1>
-        {/* Your links should go here */}
 
+        <Navlink to='/red' >Red</Navlink>
+        <Navlink to='/green' >Green</Navlink>
+        <Navlink to='/blue' >Blue</Navlink>
+        <Navlink to='/violet' >Violet</Navlink>
+        
         <div id="rainbow">
-          {/* Your routes should go here */}
+          <Route path="/red" component="{Red}"/>
+          <Route path="/green" component="{Green}"/>
+          <Route path="/blue" component="{Blue}"/>
+          <Route path="/violet" component="{Violet}"/>
         </div>
       </div>
     );
